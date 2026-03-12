c2e49d387aa982ac6d9df95192402ce27950c2af
Kanon14 <geochua144@gmail.com>
2026-03-12 14:56:04 +0800

    training constants, subject to change is added other type of training

---
+++ safetyAI/constant/training_pipeline/__init__.py
@@ -0,0 +1,31 @@
+ARTIFACTS_DIR: str = "artifacts" 
+
+"""
+Data Ingestion related constant
+"""
+DATA_INGESTION_DIR_NAME: str = "data_ingestion"
+
+DATA_INGESTION_FEATURE_STORE_DIR: str = "feature_store"
+
+ROBOFLOW_WORKSPACE = "roboflow-universe-project"
+
+ROBOFLOW_PROJECT = "personal-protective-equipment-combined-model"
+
+"""
+Data Validation related constant
+"""
+DATA_VALIDATION_DIR_NAME: str = "data_validation"
+
+DATA_VALIDATION_STATUS_FILE = "status.txt"
+
+DATA_VALIDATION_ALL_REQUIRED_FILES = ["train", "test", "valid", "data.yaml"]
+
+
+"""
+Model Trainer related constant
+"""
+MODEL_TRAINER_DIR_NAME: str = "model_trainer"
+
+MODEL_TRAINER_PRETRAINED_WEIGHT_NAME: str = "yolo26s.pt"
+
+MODEL_TRAINER_NO_EPOCHS: int = 100
